48944491e641bb7a2e5b1d7616f455bd9aeb7893
Amir Astezhev <famkapav@gmail.com>
2025-07-03 14:14:28 +0200

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,84 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+      prettier,
+    },
+    rules: {
+      // Import sorting
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Side effect imports (e.g. polyfills)
+            ['^\\u0000'],
+            // Packages. `react` related packages come first.
+            ['^react', '^@?\\w'],
+            // Absolute imports and other imports such as Vue-style `@/foo`
+            // Anything that does not start with a dot.
+            ['^[^.]'],
+            // Relative imports.
+            ['^\\.'],
+            // Style imports.
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+
+      // Code style
+      'quotes': ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'max-len': ['off'], // Disable ESLint's max-len in favor of Prettier
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error',
+      'object-curly-spacing': ['error', 'always'],
+
+      // Unused variables
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_',
+        },
+      ],
+      'no-unused-vars': 'off', // Turn off base rule as it conflicts with TypeScript rule
+
+      // Prettier autofix
+      'prettier/prettier': 'error',
+
+      // React specific
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]) 
